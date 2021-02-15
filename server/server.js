e2b52db407fb4ffdddc12d2ba1010c6971014fa4
user <james.jmson@gmail.com>
2021-02-15 06:46:55 +0100

    Change location of client WebSocket code
    Move WebSocket code from ~/.../routes/index.js to
    ~/.../views/index.ejs's embedded script.
    
    Such code should indeed be part of the downloaded index.ejs page.
    
    Add client tracking to ~/.../server/server.js
    
    Expand client WebSocket code

--- server/server.js
+++ server/server.js
@@ -4,10 +4,28 @@ const WebSocketServer=require('ws').Server;
 const wss=new WebSocketServer({
     port: 8181
 });
+const uuid=require('uuid');
 
+const clients=[];
+
+//This is executed once at every new connection.
 wss.on('connection', ws=> {
-    console.log('Client connected.');
-    ws.on('message', msg=> {
-        console.log(msg);
+    //If the client stops running and then reconnects, that counts as a new connection.
+    const clientUuid=uuid.v4();
+    clients.push({
+        id: clientUuid,
+        ws: ws
     });
+    console.log(`Client ${clientUuid} connected.`);
+
+    ws.on('message', msg=> {
+        for(let i=0; i<clients.length; ++i){
+            const clientSocket=clients[i].ws;
+            // console.log(`Client ${clients[i].id}: ${message}`);
+            clientSocket.send(JSON.stringify({
+                id: clientUuid,
+                message: msg
+            }));
+        }
+    })
 });
