70a025e9c1b667a46cfba7d15c9df39d0e3460ca
user <james.jmson@gmail.com>
2021-02-17 05:17:10 +0100

    Update server
    - Add nick reassignement feature.
    
    - Alert of client disconnection.
    
    - Alert of server disconnection.

--- server/server.js
+++ server/server.js
@@ -1,31 +1,85 @@
 'use strict';
 
-const WebSocketServer=require('ws').Server;
+const WebSocket=require('ws');
+const WebSocketServer=WebSocket.Server;
 const wss=new WebSocketServer({
-    port: 8181
+    port: 8080
 });
 const uuid=require('uuid');
 
 const clients=[];
 
-//This is executed once at every new connection.
+function wsSend(type, clientUuid, nickname, message){
+    for(let i=0; i<clients.length; ++i){//No point in for/of as it creates another variable ans is less readable.
+        const clientSocket=clients[i].ws;
+        if(clientSocket.readyState===WebSocket.OPEN){//Send message to all available clients.
+            clientSocket.send(JSON.stringify({
+                type: type,
+                id: clientUuid,//Message is related to this client.
+                nickname: nickname,
+                message: message
+            }));
+        }
+    }
+}
+
+let clientIndex=1;
+
 wss.on('connection', ws=> {
-    //If the client stops running and then reconnects, that counts as a new connection.
     const clientUuid=uuid.v4();
-    clients.push({
+    let nickname='Anonymous'+clientIndex;
+    clientIndex+=1;
+    clients.push({//Have a subobject to send via weSend.//This object should be made available locally.
         id: clientUuid,
-        ws: ws
+        ws: ws,
+        nickname: nickname
     });
     console.log(`Client ${clientUuid} connected.`);
 
-    ws.on('message', msg=> {
+    const connect_message=nickname+' has connected';
+    wsSend('notification', clientUuid, nickname, connect_message);
+
+    ws.on('message', message=> {
+        message=JSON.parse(message);
+        message=message.message;
+        //Use switch or refer to Discord bot.
+        if(message.indexOf('/nick')==0){
+            message=message.trim();//'/nick  James ' -> '/nick  James'.
+            const nicknameArray=message.split(' ');//['/nick', '', 'James'].
+            let responseMessage=`Nick reassignement unsuccesful.`;
+            if(nicknameArray.length>=2){
+                const old_nickname=nickname;
+                nickname=nicknameArray.pop();//'James'.
+                for(let i=0; i<clients.length; ++i){//Update clients array.
+                    if(clients.nickname==old_nickname){
+                        clients.nickname=nickname;
+                        break;
+                    }
+                }
+                responseMessage=`Client ${old_nickname} changed to ${nickname}.`;
+            }
+            wsSend('nick_update', clientUuid, nickname, responseMessage);
+            //Having this in a function simplifies it with return statements.
+        }
+        else{
+            wsSend('message', clientUuid, nickname, message);
+        }
+    });
+
+    ws.on('close', ()=> {
+        const disconnectMessage=`${nickname} has left the chat.`;
+        wsSend('notification', clientUuid, nickname, disconnectMessage);
         for(let i=0; i<clients.length; ++i){
-            const clientSocket=clients[i].ws;
-            // console.log(`Client ${clients[i].id}: ${message}`);
-            clientSocket.send(JSON.stringify({
-                id: clientUuid,
-                message: msg
-            }));
+            if(clients[i].id==clientUuid){
+                clients.splice(i, 1);
+                break;
+            }
         }
     })
+});
+
+process.on('SIGINT', ()=> {
+    console.log('Shutting down server');
+    wsSend('notification', null, null, 'Server is down.');//Improve wsSend() structure.
+    process.exit();
 });
