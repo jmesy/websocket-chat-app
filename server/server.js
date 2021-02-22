817e132f0e2b4a19d87e3dbcfcd071af7332d3ef
user <james.jmson@gmail.com>
2021-02-22 05:07:49 +0100

    Refactor server

--- server/server.js
+++ server/server.js
@@ -1,88 +1,124 @@
 'use strict';
 
+const fs=require('fs');
 const WebSocket=require('ws');
 const WebSocketServer=WebSocket.Server;
 const wss=new WebSocketServer({
     port: 8080
 });
 const uuid=require('uuid');
+const path=require('path');
 
-const clients=[];
-
-function wsSend(type, clientUuid, nickname, message){
-    for(let i=0; i<clients.length; ++i){//No point in for/of as it creates another variable and is less readable.
-        const clientSocket=clients[i].ws;
-        if(clientSocket.readyState===WebSocket.OPEN){//Send message to all available clients.
+function wsSend(resObject){
+    for(const client in clients){
+        const clientSocket=clients[client].ws;
+        if(clientSocket.readyState==WebSocket.OPEN){
             clientSocket.send(JSON.stringify({
-                type: type,
-                id: clientUuid.substr(0, 8),//Message is related to this client.
+                type: resObject.type,
+                id: resObject.clientUuid,//
                 props: {
-                    nickname: nickname,
-                    message: message
+                    nickname: resObject.nickname,//
+                    message: resObject.message
                 }
             }));
         }
     }
 }
 
-let clientIndex=1;
+//Load commands in the commands object.
+const commands={};
+fs
+    .readdirSync(path.join(__dirname, 'commands'))//Use async.
+    .forEach(val=> {
+        const command=require(path.join(__dirname, `commands/${val}`));
+        commands[command.name]=command;
+    });
+
+const clients={};
 
 wss.on('connection', ws=> {
     const clientUuid=uuid.v4();
-    // let nickname='Anonymous'+clientIndex;
     let nickname=clientUuid.substring(0, 8);
-    clientIndex+=1;
-    clients.push({//Have a subobject to send via weSend.//This object should be made available locally.
+    clients[clientUuid]={
         id: clientUuid,
         ws: ws,
         nickname: nickname
+    };
+    // console.log(`Client ${clientUuid} connected.`);
+
+    const connectMessage=nickname+' has connected';
+    wsSend({
+        type: 'notification',
+        id: clientUuid,
+        nickname: clients[clientUuid].nickname,
+        message: connectMessage
     });
-    console.log(`Client ${clientUuid} connected.`);
 
-    const connect_message=nickname+' has connected';
-    wsSend('notification', clientUuid, nickname, connect_message);
-
-    ws.on('message', message=> {
-        message=JSON.parse(message);
-        message=message.message;
-        //Use switch or refer to Discord bot.
-        if(message.indexOf('/nick')==0){
-            message=message.trim();//'/nick  James ' -> '/nick  James'.
-            const nicknameArray=message.split(' ');//['/nick', '', 'James'].
-            let responseMessage=`Nick reassignement unsuccesful.`;
-            if(nicknameArray.length>=2){
-                const old_nickname=nickname;
-                nickname=nicknameArray.pop();//'James'.
-                for(let i=0; i<clients.length; ++i){//Update clients array.
-                    if(clients.nickname==old_nickname){
-                        clients.nickname=nickname;
-                        break;
-                    }
-                }
-                responseMessage=`Client ${old_nickname} changed to ${nickname}.`;
+    const handleCommand=function(message){
+        const args=message
+            .substring(prefix.length)
+            .split(/ +/);
+        const commandName=args
+            .shift()
+            .toLowerCase();
+        // console.log(commandName, args);
+        if(!args.length){
+            return 'No arguments have been provided.';
+        }
+        // console.log(commands);
+        if(commands.hasOwnProperty(commandName)){
+            const command=commands[commandName]
+            const res=command.execute(args, clients[clientUuid]);
+            //Update changes to client.
+            for(const property in res.client){
+                clients[clientUuid][property]=res.client[property];
             }
-            wsSend('nick_update', clientUuid, nickname, responseMessage);
-            //Having this in a function simplifies it with return statements.
+            return res['Message'];
+        }
+    }
+
+    const prefix='/';
+    ws.on('message', msg=> {
+        const msgObj=JSON.parse(msg);
+        const {message}=msgObj;
+        if(message.startsWith(prefix)){
+            const resMessage=handleCommand(message);
+            wsSend({
+                type: 'notification',
+                id: clientUuid,
+                nickname: clients[clientUuid].nickname,
+                message: resMessage
+            });
         }
         else{
-            wsSend('message', clientUuid, nickname, message);
+            wsSend({
+                type: 'message',
+                id: clientUuid,
+                nickname: clients[clientUuid].nickname,
+                message: message
+            });
         }
     });
 
     ws.on('close', ()=> {
         const disconnectMessage=`${nickname} has left the chat.`;
-        wsSend('notification', clientUuid, nickname, disconnectMessage);
-        for(let i=0; i<clients.length; ++i){
-            if(clients[i].id==clientUuid){
-                clients.splice(i, 1);
-                break;
-            }
-        }
+        delete clients[clientUuid];
+        wsSend({
+            type: 'notification',
+            id: clientUuid,
+            nickname: clients[clientUuid].nickname,
+            message: disconnectMessage
+        });
     })
 });
 
 process.on('SIGINT', ()=> {
     console.log('Shutting down server');
-    wsSend('notification', null, null, 'Server is down.');//Improve wsSend() structure.
+    wsSend({
+        type: 'notification',
+        id: null,
+        nickname: null,
+        message: 'Server is down.'
+    });
     process.exit();
 });
